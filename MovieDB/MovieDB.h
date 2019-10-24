6cc16a64a9e58fa2435ef0939dbc5f7e64a06282
Matt Ginelli <matt_uhs@me.com>
2019-10-23 16:08:12 -1000

    Initial Commit

---
+++ MovieDB/MovieDB.h
@@ -0,0 +1,19 @@
+//
+//  MovieDB.h
+//  MovieDB
+//
+//  Created by Matthew Ginelli on 10/23/19.
+//  Copyright © 2019 MGinelli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieDB.
+FOUNDATION_EXPORT double MovieDBVersionNumber;
+
+//! Project version string for MovieDB.
+FOUNDATION_EXPORT const unsigned char MovieDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieDB/PublicHeader.h>
+
+
